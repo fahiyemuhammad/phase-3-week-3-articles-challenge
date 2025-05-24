0c055771f8f9099e9f5b0067b843444cfe3f013e
Fahiye Muhammad <fahiye254@gmail.com>
2025-05-24 21:44:48 +0300

    added a method for creating a new article and saving it to the database

--- code-challenge/tests/test_magazine.py
+++ code-challenge/tests/test_magazine.py
@@ -17,3 +17,22 @@ def test_magazine_articles_returns_articles():
     if magazines:
         articles = magazines[0].articles()
         assert isinstance(articles, list)
+
+
+def add_article(self, magazine, title):
+    import sqlite3
+    from lib.models.article import Article
+
+    conn = sqlite3.connect('lib/db/database.db')
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "INSERT INTO articles (title, content, author_id, magazine_id) VALUES (?, ?, ?, ?)",
+        (title, '', self.id, magazine.id)  # Empty content for now
+    )
+
+    conn.commit()
+    article_id = cursor.lastrowid
+    conn.close()
+
+    return Article(id=article_id, title=title, content='', author_id=self.id, magazine_id=magazine.id)
